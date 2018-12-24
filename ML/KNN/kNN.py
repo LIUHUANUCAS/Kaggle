5d84aa30853a612e4e14e72c9a1478236b36ed69
LIUHUAN <happygodliu@tencent.com>
2018-12-24 18:55:28 +0800

    [update]knn load data function

--- ML/KNN/kNN.py
+++ ML/KNN/kNN.py
@@ -1,4 +1,4 @@
-from numpy import *
+import numpy as np
 import operator
 
 def createDataSet():
@@ -21,3 +21,26 @@ def classify0(inX,dataSet,labels,k):
         classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1 
     sortedClassCount = sorted(classCount.iteritems(),key=operator.itemgetter(1),reverse=True)
     return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    label_ = {'didntLike':0,'largeDoses':1,'smallDoses':2}
+    data = []
+    label = []
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.rstrip()
+            e = line.split('\t')
+            money = float(e[0])
+            play_time = float(e[1])
+            ice = float(e[2])
+            data.append([money,play_time,ice])
+            label.append(e[-1])
+            #label.append(label_[e[-1]])
+
+    return np.array(data),np.array(label)
+
+#data,label = file2matrix('./datingTestSet.txt')
+#print len(data),len(label)
+#
+#print data[0][0] + 1
+#print label[0]
