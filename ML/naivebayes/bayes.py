d8e088680e432e176c4d24972f81baadc400fac2
LIUHUAN <happygodliu@tencent.com>
2019-01-03 10:07:38 +0800

    [update]naive bayes for spam email classification

--- ML/naivebayes/bayes.py
+++ ML/naivebayes/bayes.py
@@ -66,3 +66,75 @@ def testingNB():
     testEntry = ['stupid','garbage']
     thisdoc = array(setOfWords2Vec(vocabList,testEntry))
     print(testEntry,'classified as',classifyNB(thisdoc,p0V,p1V,pAb))
+
+
+# count occurency of word in document
+def bagOfWords2VecMN(vocabList,inputSet):
+    vec = [0] * len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            vec[vocabList.index(word)] += 1
+    return vec
+
+def textParse(text):
+    import re
+    listword = re.split(r'\W*',text)
+    return [w.lower() for w in listword if len(w) > 2]
+
+def spamTest():
+    doclist = []
+    label = []
+    fulltext = []
+    for i in range(1,26):
+        wordlist = textParse(open('./email/spam/%d.txt'%i).read())
+        doclist.append(wordlist)
+        fulltext.extend(wordlist)
+        label.append(1)
+        wordlist = textParse(open('./email/ham/%d.txt'%i).read())
+        doclist.append(wordlist)
+        fulltext.extend(wordlist)
+        label.append(0)
+    
+    # get vocabulary for all doc
+    vocablist = createVocabList(doclist)
+
+    trainingSet = range(50)
+    testSet = []
+    uniqmap = {}
+    for i in range(50):
+
+        randindex = int(random.uniform(0,len(trainingSet)))
+        uniqmap[randindex] = 1
+        if len(uniqmap) >= 10 :
+            break
+        # testSet.append(trainingSet[randindex])
+        # del(trainingSet[randindex])
+    for k in uniqmap:
+        testSet.append(trainingSet[k])
+
+    word2vec = bagOfWords2VecMN
+    trainMat = []
+    trainClass = []
+    for docindex in trainingSet:
+        if docindex in uniqmap:# skip test data
+            continue
+        trainMat.append(word2vec(vocablist,doclist[docindex]))
+        trainClass.append(label[docindex])
+    
+    # train NB
+    p0v ,p1v,pSpam = trainNB0(array(trainMat),array(trainClass))
+
+    errcount = 0
+    # test NB
+    for docindex in testSet:
+        wordvec = word2vec(vocablist,doclist[docindex])
+        classlabel = classifyNB(array(wordvec),p0v,p1v,pSpam)
+        if classlabel != label[docindex]:
+            errcount += 1
+            print(doclist[docindex])
+    
+    print('err rate:',float(errcount)/ len(testSet))
+
+        
+
+
